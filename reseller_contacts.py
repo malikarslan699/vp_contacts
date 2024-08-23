13e11dc147568204b042f17d029deff0b718d850
malikarslan699 <46280359+malikarslan699@users.noreply.github.com>
2024-08-23 11:51:14 +0400

    Update reseller_contacts.py

--- reseller_contacts.py
+++ reseller_contacts.py
@@ -1,5 +1,17 @@
 # Reseller phone numbers
 reseller_list = {
-    "alamin4aa": "+971525632018",
+    "itelpro": "+971552151450",
+    "alamin4aa": "+8801714902277",
+    "alamin3aa": "+8801714902277",
+    "mohsin": "+923330007570",
+    "omar ": "+8801402064382",
+    "omar99": "+8801402064382",
+    "rasel14": "+971563548389",
+    "rasel23": "+971563548389",
+    "shopan8": "+971553968629",
+    "shopan99": "+971553968629",
+    "hadi": "+971557898097",
+    "rwattoo": "+971557898097",
+    "wattoo ": "+971557898097",
     # Add more resellers as needed
 }
